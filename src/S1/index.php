1b396c94612531cfb7f627fa74215114361581f4
LynAguayo <lyn.aguayoc@gmail.com>
2025-09-30 16:23:25 +0200

    update exercises

---
+++ src/S1/index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <title>S1: Ejercicios PHP</title>
+</head>
+<body>
+    <h1>Sesión 1: ejercicios PHP</h1>
+    <ul>
+        <li><a href="S1/ejercicio1.php">Ejercicio 1</a></li>
+        <li><a href="S1/ejercicio2.php">Ejercicio 2</a></li>
+        <li><a href="S1/ejercicio1.php">Ejercicio 3</a></li>
+    </ul>
+</body>
+</html>
---
+++ src/S3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <title>S3: Ejercicios PHP</title>
+</head>
+<body>
+    <h1>Sesión 3: ejercicios PHP</h1>
+    <ul>
+        <li><a href="S3/ejercicio1.php">Ejercicio 1</a></li>
+        <li><a href="S3/ejercicio2.php">Ejercicio 2</a></li>
+        <li><a href="S3/ejercicio1.php">Ejercicio 3</a></li>
+    </ul>
+</body>
+</html>
